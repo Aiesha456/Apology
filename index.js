efc850ac1b75a134164218cda4335d006bad222e
Aiesha456 <aieshashaiku123@gmail.com>
2024-05-31 16:00:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//
+
+document.querySelector("button").addEventListener("click",function(){
+    alert("I am sorry dattuuu..Please forgive me :(");
+
+});
+
+document.querySelector("button").addEventListener("click",function(){
+    document.querySelector(".main").innerHTML="Love youu";
+});
